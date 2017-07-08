501cb1fe1538ac7be11c6eb955143996aedce278
anton mednonogov <anton.mednonogov@gmail.com>
2017-07-09 00:40:11 +0300

    Add some code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace dotnetcore_redis_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("0.0.0.0");
+            var database = connection.GetDatabase();
+            
+            database.StringSet("greeting", "Hello World!");
+            Console.WriteLine(database.StringGet("greeting"));
+        }
+    }
+}
